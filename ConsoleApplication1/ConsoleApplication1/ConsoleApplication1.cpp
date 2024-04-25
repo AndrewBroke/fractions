6126243f1483dca5f9e2f2057e54842030121f5d
AndrewBroke <kuzukov-work@yandex.ru>
2024-04-25 19:32:36 +0700

    smth

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 /// <summary>
 /// num - числитель
 /// denum - знаменатель
@@ -8,12 +10,42 @@ class Fraction {
 private:
     int num; 
     int denum;
+
+public:
+    // Конструктор с параметрами;
+    Fraction(int n1, int n2) 
+    {
+        num = n1;
+        denum = n2;
+        reduct();
+    }
+
+    // Конструктор по умолчанию
+    Fraction() 
+    {
+        num = 1;
+        denum = 1;
+    }
+
+    void print() 
+    {
+        cout << num << "/" << denum << endl;
+    }
+
+    // Функция сокращения дроби
+    // Вызывать ее в конструкторе
+    void reduct() 
+    {
+        // TODO
+
+    }
 };
 
 
 using namespace std;
 int main()
 {
-    
-    
+    Fraction frac = Fraction(4, 10); // -> 2/5
+    Fraction frac1;
+    frac.print();
 }
